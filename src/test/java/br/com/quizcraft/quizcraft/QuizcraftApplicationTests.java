1175ec2c6861f0f431f33b9533b5a0e676c5b053
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-08-21 20:57:18 -0300

    test: create initial test configutation

--- src/test/java/br/com/quizcraft/quizcraft/QuizcraftApplicationTests.java
+++ src/test/java/br/com/quizcraft/quizcraft/QuizcraftApplicationTests.java
@@ -2,8 +2,13 @@ package br.com.quizcraft.quizcraft;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(
+		locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
 class QuizcraftApplicationTests {
 
 	@Test
---
+++ src/test/java/br/com/quizcraft/quizcraft/uploadedfile/controller/UploadFileControllerTest.java
@@ -0,0 +1,46 @@
+package br.com.quizcraft.quizcraft.uploadedfile.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest
+class UploadFileControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Should upload file")
+    void shouldUploadFile() throws Exception {
+        mockMvc.perform(post("/upload")
+                        .contentType(MediaType.APPLICATION_PDF))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @DisplayName("Should not upload file because content type is not provided")
+    void shouldUploadFileBecauseContentTypeIsNotProvided() throws Exception {
+        mockMvc.perform(post("/upload"))
+                .andExpect(MockMvcResultMatchers.status().isUnsupportedMediaType());
+    }
+
+    @Test
+    @DisplayName("Should not upload file because file is not provided")
+    void shouldNotUploadFile() {
+        // TODO
+    }
+
+    @Test
+    @DisplayName("Should not upload file because file is not correct type")
+    void shouldNotUploadFileBecauseFileIsNotCorrectType() {
+        // TODO
+    }
+}
