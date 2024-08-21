22a00d5392511dbeae424ef3d4adcefff0938875
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-08-21 18:40:18 -0300

    feat: first items

---
+++ src/main/java/br/com/quizcraft/quizcraft/QuizcraftApplication.java
@@ -0,0 +1,13 @@
+package br.com.quizcraft.quizcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizcraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/quizcraft/quizcraft/QuizcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quizcraft.quizcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
