286f5c2c297a59f32f2d9b3eb435e523e4d74d6c
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-08-22 18:08:46 -0300

    feat: add service in controller

--- src/main/java/br/com/quizcraft/uploadedfile/controller/UploadFileController.java
+++ src/main/java/br/com/quizcraft/uploadedfile/controller/UploadFileController.java
@@ -1,17 +1,32 @@
 package br.com.quizcraft.uploadedfile.controller;
 
+import br.com.quizcraft.uploadedfile.service.uploadfile.UploadFileServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class UploadFileController {
 
+    @Autowired
+    private UploadFileServiceInterface uploadFileService;
+
     @PostMapping(value = "/upload",
-            consumes = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity uploadFile() {
-        // TODO
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> uploadFile(
+            @RequestParam(name = "exam") MultipartFile exam,
+            @RequestParam(name = "answers") MultipartFile answers
+            ) throws IOException {
+
+        uploadFileService.uploadFile(exam, answers);
+
         return ResponseEntity.ok().build();
     }
 }
