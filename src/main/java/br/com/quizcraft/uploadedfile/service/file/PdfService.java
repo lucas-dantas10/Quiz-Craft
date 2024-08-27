bf7505903d6bbc1696683ef1cfb40948d6cfce1c
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-08-26 23:28:04 -0300

    feat: add questions in table

--- src/main/java/br/com/quizcraft/uploadedfile/service/file/PdfService.java
+++ src/main/java/br/com/quizcraft/uploadedfile/service/file/PdfService.java
@@ -1,16 +1,20 @@
 package br.com.quizcraft.uploadedfile.service.file;
 
+import br.com.quizcraft.question.dto.QuestionDto;
+import br.com.quizcraft.question.entity.Question;
+import br.com.quizcraft.question.enums.AnswerEnum;
+import br.com.quizcraft.question.repository.QuestionRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,20 +22,24 @@ import java.util.regex.Pattern;
 @Slf4j
 public class PdfService {
 
+    @Autowired
+    private QuestionRepository questionRepository;
+
     public void extractQuestionTextFromPdf(MultipartFile exam) throws IOException {
         String questions = this.getTextFromPdf(exam);
 
+        // TODO: Refator code
+
         List<Map<String, Object>> questionsMap = new ArrayList<>();
-        String[] questionSplit = questions.split("QUESTÃO \\d+"); // TODO: improve regex to get question without options
+        questions = questions.replaceAll("(?i)\\b(caderno\\s*\\w+|1º DIA|.*fragmento).*\\R?", "");
+        String[] questionSplit = questions.split("QUESTÃO \\d+");
 
         for (String question : questionSplit) {
             if (question.isEmpty()) continue;
 
             Map<String, Object> questionMap = new HashMap<>();
-            // TODO: improve options to get ONLY option and not in text question
             Pattern patternOption = Pattern.compile("([A-E])\\s(.+?)(?=(?:\\n[A-E]\\s)|$)", Pattern.DOTALL);
             Matcher matcherOption = patternOption.matcher(question);
-            log.info("Question: {}", question);
 
             List<String> options = new ArrayList<>();
             int startOptionsIndex = question.length();
@@ -40,16 +48,34 @@ public class PdfService {
                 options.add(matcherOption.group(1) + ": " + matcherOption.group(2).trim());
                 startOptionsIndex = Math.min(startOptionsIndex, matcherOption.start());
             }
-            log.info("Options: {}", options);
 
             String questionText = question.substring(0, startOptionsIndex).trim();
-            questionMap.put("Enunciado", questionText);
-            questionMap.put("Opções", options);
+            questionMap.put("question_text", questionText);
+            questionMap.put("options", options);
 
             questionsMap.add(questionMap);
         }
-        
-        // TODO: save in DTO and return DTO
+
+        // TODO: Refator code
+        questionsMap.forEach(item -> {
+            ObjectMapper mapper = new ObjectMapper();
+            String options = "";
+            try {
+                options = mapper.writeValueAsString(item.get("options"));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+
+            Question question = new Question();
+            question.setQuestionText((String) item.get("question_text"));
+            question.setOptions(options);
+            question.setCorrectAnswer(AnswerEnum.A);
+            questionRepository.insertQuestion(
+                question.getCorrectAnswer().toString(),
+                question.getOptions(),
+                question.getQuestionText()
+            );
+        });
     }
 
     public void extractAnswersTextFromPdf(MultipartFile answers) throws IOException {
