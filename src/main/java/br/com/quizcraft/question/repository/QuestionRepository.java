0e6fefb976eeb98df615f22d4a7fa7e8536cd23a
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-08-26 23:27:02 -0300

    feat: add custom query

--- src/main/java/br/com/quizcraft/question/repository/QuestionRepository.java
+++ src/main/java/br/com/quizcraft/question/repository/QuestionRepository.java
@@ -1,9 +1,24 @@
 package br.com.quizcraft.question.repository;
 
 import br.com.quizcraft.question.entity.Question;
+import br.com.quizcraft.question.enums.AnswerEnum;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, UUID> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO tb_questions (question_id, correct_answer, options, question_text) " +
+        "VALUES (uuid_generate_v4(), :correctAnswer, to_json(:options), :questionText)", nativeQuery = true)
+    void insertQuestion(@Param("correctAnswer") String correctAnswer,
+                @Param("options") String options,
+                @Param("questionText") String questionText);
 }
