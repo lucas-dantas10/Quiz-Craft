68c5e8b1ec43b5a7d5617ccc02ecbc17a7c36ae1
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-08-27 21:47:28 -0300

    feat: add option model

---
+++ src/main/java/br/com/quizcraft/question/model/Option.java
@@ -0,0 +1,20 @@
+package br.com.quizcraft.question.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"option", "text"})
+public class Option {
+
+    private String option;
+    private String text;
+}
