9efdd07196b9d9e3a4f8d8193801a272fb4c95e0
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-08-28 19:22:28 -0300

    fix: adjust method

--- src/main/java/br/com/quizcraft/question/builder/OptionBuilder.java
+++ src/main/java/br/com/quizcraft/question/builder/OptionBuilder.java
@@ -2,16 +2,25 @@ package br.com.quizcraft.question.builder;
 
 import br.com.quizcraft.question.model.Option;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class OptionBuilder {
 
-    public static List<Option> buildOption(Map<String, String> option) {
-        return Collections.singletonList(Option.builder()
-            .option(option.get("option"))
-            .text(option.get("text"))
-            .build());
+    public static List<Option> buildOption(List<Map<String, String>> options) {
+        List<Option> optionsList = new ArrayList<>();
+
+        options.forEach(option -> {
+            optionsList.add(
+                Option.builder()
+                .option(option.get("option"))
+                .text(option.get("text"))
+                .build()
+            );
+        });
+
+        return optionsList;
     }
 }
